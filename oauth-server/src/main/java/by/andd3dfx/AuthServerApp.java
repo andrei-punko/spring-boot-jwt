931e526efa0d42e40b0bb94987eb18dc41b305bd
Andrei Punko <andd3dfx@gmail.com>
2020-03-09 10:45:07 +0300

    Add Main sprig-boot classes to run apps

---
+++ oauth-server/src/main/java/by/andd3dfx/AuthServerApp.java
@@ -0,0 +1,12 @@
+package by.andd3dfx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthServerApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthServerApp.class, args);
+	}
+}
