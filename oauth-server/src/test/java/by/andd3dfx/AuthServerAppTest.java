cb06df7b51ec61d941be38b775edc70ee9d7bfd1
Andrei Punko <andd3dfx@gmail.com>
2020-03-10 12:45:59 +0300

    Add missed tests

---
+++ oauth-server/src/test/java/by/andd3dfx/AuthServerAppTest.java
@@ -0,0 +1,12 @@
+package by.andd3dfx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServerAppTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
