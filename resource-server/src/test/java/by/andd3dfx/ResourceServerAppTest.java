b6f092c50faef159299a5bd2c80687499a769295
Andrei Punko <andd3dfx@gmail.com>
2020-03-10 14:54:19 +0300

    Add missed annotations. Use @Test from org.junit instead of jupiter

--- resource-server/src/test/java/by/andd3dfx/ResourceServerAppTest.java
+++ resource-server/src/test/java/by/andd3dfx/ResourceServerAppTest.java
@@ -1,12 +1,15 @@
 package by.andd3dfx;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
-class ResourceServerAppTest {
+public class ResourceServerAppTest {
 
     @Test
-    void main() {
+    public void contextLoads() {
     }
 }
